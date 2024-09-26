e385a390eae88f9c194cbb9a2150f62b84cb6dc9
Victor Alves da Costa <101835324+Victor-Alves1@users.noreply.github.com>
2024-09-26 22:28:02 +0000

    Adicionando tabela de reservas

--- draft.py
+++ draft.py
@@ -6,7 +6,7 @@ create_table(
     database='mydatabase',
     table_name='cadastro_cliente',
     columns_desc="""
-        ID CLIENTE	INTEGER PRIMARY KEY NOT NULL,
+        ID_CLIENTE	INTEGER PRIMARY KEY NOT NULL,
         CPF	varchar(11)	NOT NULL,
         NOME varchar(100)	NOT NULL,
         DATA_DE_NASCIMENTO	varchar(20)	NOT NULL,
@@ -16,7 +16,7 @@ create_table(
 )
 
 data_cliente = pd.DataFrame({
-    'ID': [1, 2, 3, 4],
+    'ID_CLIENTE': [1, 2, 3, 4],
     'CPF': ['111', '222', '333', '4444'],
     'NOME': ['Victor', 'Tamires', 'Liliane', 'Guilherme'],
     'DATA_DE_NASCIMENTO': ['01-02-2000', '02-04-1998', '04-06-1996', '06-08-2002'],
@@ -50,6 +50,26 @@ data_quarto = pd.DataFrame({
     'Valor': [1.1,2.2,3.3,4.5]
 })
 
+create_table(
+    database='mydatabase',
+    table_name='reservas',
+    columns_desc="""
+        ID_RESERVA	INTEGER PRIMARY KEY NOT NULL,
+        ID_CLIENTE	INTEGER NOT NULL,
+        ID_QUARTO	INTEGER NOT NULL,
+        CHECKIN	varchar(20) NOT NULL,
+        CHECKOUT	varchar(20) NOT NULL
+    """
+)
+
+data_reserva = pd.DataFrame({
+    'ID_RESERVA': [1, 2, 3, 4],
+    'ID_CLIENTE': [4, 3, 2, 1],
+    'ID_QUARTO': [3, 2, 1, 4],
+    'CHECKIN': ['01-02-2024', '10-02-2024', '02-02-2024', '30-02-2024'],
+    'CHECKOUT': ['09-02-2024', '19-02-2024', '09-02-2024', '01-02-2024']
+})
+
 conn = sqlite3.connect('mydatabase.db')
 
 data_cliente.to_sql(
@@ -60,6 +80,12 @@ data_cliente.to_sql(
 
 data_quarto.to_sql(
     'cadastro_quartos', conn,
+    if_exists='replace',
+    index=False
+)
+
+data_reserva.to_sql(
+    'reservas', conn,
     if_exists='replace',
     index=False
 )
@@ -76,11 +102,18 @@ query_quartos = """
     SELECT * 
     FROM cadastro_quartos
 """
+query_reservas = """ 
+    SELECT * 
+    FROM reservas
+"""
 
 select_clientes = pd.read_sql_query(query_clientes, conn)
 print(select_clientes)
 print('------------------------------------------------------------------')
 select_quartos = pd.read_sql_query(query_quartos, conn)
 print(select_quartos)
+print('------------------------------------------------------------------')
+select_reservas = pd.read_sql_query(query_reservas, conn)
+print(select_reservas)
 
 conn.close()
